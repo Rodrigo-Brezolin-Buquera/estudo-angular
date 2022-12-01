05d8842d9cd5bead146fe2feb104d0720c9c4491
RODRIGO BREZOLIN BUQUERA <81428197+Rodrigo-Brezolin-Buquera@users.noreply.github.com>
2022-12-01 19:55:19 -0300

    bug versão - salvar antes de tentar algo

--- memoteca/src/app/componentes/pensamentos/lista-pensamento/lista-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/lista-pensamento/lista-pensamento.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
 
 @Component({
   selector: 'app-lista-pensamento',
   templateUrl: './lista-pensamento.component.html',
   styleUrls: ['./lista-pensamento.component.css']
 })
-export class ListaPensamentoComponent {
+
+export class ListaPensamentoComponent implements OnInit {
 
   listaPensamentos:Pensamento[] = []
 
+  constructor(private service: PensamentoService){ }
+ 
+
+  ngOnInit():void {
+    this.service.listar()
+  }
 }
